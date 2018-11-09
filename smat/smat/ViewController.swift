ee44ad50c9fdbc9197fba9a07e64cd24e49303d8
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-11-10 01:43:39 +0900

    add: addInputFiled

--- smat/smat/ViewController.swift
+++ smat/smat/ViewController.swift
@@ -9,13 +9,33 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate  {
 
+    @IBOutlet weak var roomNumber: UITextField!
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        roomNumber.becomeFirstResponder()
+        roomNumber.delegate = self
     }
-
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text?.count == 3 {
+            print("s")
+            return true
+        } else {
+            print("ss")
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        testLabel.text = textField.text
+    }
+    @IBAction func roomButton(_ sender: UIButton) {
+        testLabel.text = "textField.text"
+    }
+    
 }
