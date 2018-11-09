a4ed2fa38ef0caebc0e9314a11cda6e0d613086e
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-11-09 14:40:24 +0900

    add: create-app

---
+++ smat/smat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  smat
+//
+//  Created by Hiroshi Tamura on 2018/11/09.
+//  Copyright © 2018 KINC. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
