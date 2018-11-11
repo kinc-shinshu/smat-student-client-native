56b7c7f02805fcf978fc04e0353a0ff9e28a0ec6
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-11-12 01:59:14 +0900

    add: addNavigationButNotGood

---
+++ smat/Pods/Target Support Files/Pods-smatUITests/Pods-smatUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smatUITests : NSObject
+@end
+@implementation PodsDummy_Pods_smatUITests
+@end
---
+++ smat/smat/QuestionsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  QuestionsTableViewCell.swift
+//  smat
+//
+//  Created by Hiroshi Tamura on 2018/11/12.
+//  Copyright © 2018 KINC. All rights reserved.
+//
+
+import UIKit
+import iosMath
+
+class QuestionsTableViewCell: UITableViewCell {
+    var receiveRoomNumber:String?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
