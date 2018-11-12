30aee7f02517ad531937a9f254305b15379dbd28
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-11-12 22:00:53 +0900

    add: addQuestionClass

---
+++ smat-new/smat-new/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  smat-new
+//
+//  Created by Hiroshi Tamura on 2018/11/12.
+//  Copyright © 2018 弘田村. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
