510bfbb55177f30ac9d2f7650bd4b63b9ef1d427
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2018-11-14 20:17:59 +0900

    add: addJsonTemplate

--- smat/smat/ViewController.swift
+++ smat/smat/ViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UITextFieldDelegate  {
 
+    @IBOutlet weak var testFile: UILabel!
     @IBOutlet weak var roomNumber: UITextField!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //  Alamofire.request(method: Method, URLString: URL)
+    func getArticles() {
+        Alamofire.request("https://smat-api.herokuapp.com/rooms/168/questions").responseJSON {response in
+            guard let object = response.result.value else {
+                return
+            }
+            var articles: [[String: String?]] = []
+            let json = JSON(object)
+            json.forEach { (_, json) in
+                let article: [String: String?] = [
+                    "questionText": json["text"].string,
+                    "questionAnswer": json["answer"].string,
+                    "questionId": json["id"].string
+                ]
+                articles.append(article)
+                self.questionLabel.text = article["questionText"] as? String
+            }
+            print(articles)
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +44,7 @@ class ViewController: UIViewController, UITextFieldDelegate  {
         roomNumber.becomeFirstResponder()
         roomNumber.delegate = self
         roomNumber.keyboardType = UIKeyboardType.numberPad
+        getArticles()
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
